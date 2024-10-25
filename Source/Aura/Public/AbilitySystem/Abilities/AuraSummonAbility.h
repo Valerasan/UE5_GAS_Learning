e8cb0337fad84d7ee8cc8248671335d93acf63da
Valerasan <yarovoy.valera2002@gmail.com>
2024-10-25 21:27:18 +0300

    add summon ability (not finished)

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraSummonAbility.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraSummonAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraSummonAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Summoning")
+	TArray<FVector> GetSpawnLocations();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Summoning")
+	int32 NumMinions = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Summoning")
+	TArray<TSubclassOf<APawn>> MinionClasses;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Summoning")
+	float MinSpawnDistance = 50.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Summoning")
+	float MaxSpawnDistance = 250.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Summoning")
+	float SpawnSpread = 90.f;
+	
+	
+};
